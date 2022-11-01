d17bd19f72edd5a80d2e4c5015b3dc53392af981
fernanda freitas <idea.fernanda@gmail.com>
2022-11-01 12:19:53 +0000

    adjusted styles

--- src/components/Flat/index.jsx
+++ src/components/Flat/index.jsx
@@ -4,7 +4,13 @@ const Flat = ( {price, name, imageUrl} ) => {
   return (
     <div className='flat'>
       <img src={imageUrl} className="flat-picture" alt="The flat" />
-      <div className='flat-title'><strong>{price}</strong> - {name}</div>
+      <div className='flat-title'>
+        <span>{name}</span>
+      </div>
+      <div className='flat-price'>
+        <span className='price'>€ {price}</span>
+        <span>noite</span>
+      </div>
     </div>
   )
 }
